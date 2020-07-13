2755947a1cfbb1600631fff31d445d0e81c162b3
Yosef <yosefardhitowin@gmail.com>
2020-07-13 19:57:46 +0200

    object oriented style

--- nerindo/ner.py
+++ nerindo/ner.py
@@ -1,199 +1,218 @@
-import numpy as np
+import time
 import torch
 from torch import nn
 from torch.optim import Adam
 from torchtext.data import Field, BucketIterator
 from torchtext.datasets import SequenceTaggingDataset
-import time
 from spacy.lang.id import Indonesian
 
 
+class Corpus(object):
+
+    def __init__(self, input_folder, min_word_freq, batch_size):
+        # list all the fields
+        self.word_field = Field(lower=True)
+        self.tag_field = Field(unk_token=None)
+        # create dataset using built-in parser from torchtext
+        self.train_dataset, self.val_dataset, self.test_dataset = SequenceTaggingDataset.splits(
+            path=input_folder,
+            train="train.tsv",
+            validation="val.tsv",
+            test="test.tsv",
+            fields=(("word", self.word_field), ("tag", self.tag_field))
+        )
+        # convert fields to vocabulary list
+        self.word_field.build_vocab(self.train_dataset.word, min_freq=min_word_freq)
+        self.tag_field.build_vocab(self.train_dataset.tag)
+        # create iterator for batch input
+        self.train_iter, self.val_iter, self.test_iter = BucketIterator.splits(
+            datasets=(self.train_dataset, self.val_dataset, self.test_dataset),
+            batch_size=batch_size
+        )
+        # prepare padding index to be ignored during model training/evaluation
+        self.word_pad_idx = self.word_field.vocab.stoi[self.word_field.pad_token]
+        self.tag_pad_idx = self.tag_field.vocab.stoi[self.tag_field.pad_token]
+
+
 class BiLSTM(nn.Module):
-    def __init__(self,
-                 input_dim,
-                 embedding_dim,
-                 hidden_dim,
-                 output_dim,
-                 n_layers,
-                 bidirectional,
-                 dropout,
-                 pad_idx):
+
+    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim, lstm_layers,
+               emb_dropout, lstm_dropout, fc_dropout, word_pad_idx):
         super().__init__()
+        self.embedding_dim = embedding_dim
+        # LAYER 1: Embedding
+        self.embedding = nn.Embedding(
+            num_embeddings=input_dim,
+            embedding_dim=embedding_dim,
+            padding_idx=word_pad_idx
+        )
+        self.emb_dropout = nn.Dropout(emb_dropout)
+        # LAYER 2: BiLSTM
+        self.lstm = nn.LSTM(
+            input_size=embedding_dim,
+            hidden_size=hidden_dim,
+            num_layers=lstm_layers,
+            bidirectional=True,
+            dropout=lstm_dropout if lstm_layers > 1 else 0
+        )
+        # LAYER 3: Fully-connected
+        self.fc_dropout = nn.Dropout(fc_dropout)
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)  # times 2 for bidirectional
 
-        self.embedding = nn.Embedding(input_dim, embedding_dim, padding_idx=pad_idx)
+    def forward(self, sentence):
+        # sentence = [sentence length, batch size]
+        # embedding_out = [sentence length, batch size, embedding dim]
+        embedding_out = self.emb_dropout(self.embedding(sentence))
+        # lstm_out = [sentence length, batch size, hidden dim * 2]
+        lstm_out, _ = self.lstm(embedding_out)
+        # ner_out = [sentence length, batch size, output dim]
+        ner_out = self.fc(self.fc_dropout(lstm_out))
+        return ner_out
 
-        self.lstm = nn.LSTM(embedding_dim,
-                            hidden_dim,
-                            num_layers=n_layers,
-                            bidirectional=bidirectional,
-                            dropout=dropout if n_layers > 1 else 0)
+    def init_weights(self):
+        # to initialize all parameters from normal distribution
+        # helps with converging during training
+        for name, param in self.named_parameters():
+          nn.init.normal_(param.data, mean=0, std=0.1)
 
-        self.fc = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
+    def init_embeddings(self, tag_pad_idx):
+        # initialize embedding for padding as zero
+        self.embedding.weight.data[tag_pad_idx] = torch.zeros(self.embedding_dim)
 
-        self.dropout = nn.Dropout(dropout)
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
-    def forward(self, text):
-        # text = [sent len, batch size]
+class NER(object):
 
-        # pass text through embedding layer
-        embedded = self.dropout(self.embedding(text))
+    def __init__(self, model, data, optimizer_cls, loss_fn_cls):
+        self.model = model
+        self.data = data
+        self.optimizer = optimizer_cls(model.parameters())
+        self.loss_fn = loss_fn_cls(ignore_index=self.data.tag_pad_idx)
 
-        # embedded = [sent len, batch size, emb dim]
+    @staticmethod
+    def epoch_time(start_time, end_time):
+        elapsed_time = end_time - start_time
+        elapsed_mins = int(elapsed_time / 60)
+        elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+        return elapsed_mins, elapsed_secs
 
-        # pass embeddings into LSTM
-        outputs, (hidden, cell) = self.lstm(embedded)
+    def accuracy(self, preds, y):
+        max_preds = preds.argmax(dim=1, keepdim=True)  # get the index of the max probability
+        non_pad_elements = (y != self.data.tag_pad_idx).nonzero()  # prepare masking for paddings
+        correct = max_preds[non_pad_elements].squeeze(1).eq(y[non_pad_elements])
+        return correct.sum() / torch.FloatTensor([y[non_pad_elements].shape[0]])
 
-        # outputs holds the backward and forward hidden states in the final layer
-        # hidden and cell are the backward and forward hidden and cell states at the final time-step
-
-        # output = [sent len, batch size, hid dim * n directions]
-        # hidden/cell = [n layers * n directions, batch size, hid dim]
-
-        # we use our outputs to make a prediction of what the tag should be
-        predictions = self.fc(self.dropout(outputs))
-
-        # predictions = [sent len, batch size, output dim]
-
-        return predictions
-
-
-def init_weights(m):
-    for name, param in m.named_parameters():
-        nn.init.normal_(param.data, mean=0, std=0.1)
-
-
-def count_parameters(m):
-    return sum(p.numel() for p in m.parameters() if p.requires_grad)
-
-
-def categorical_accuracy(preds, y, tag_pad_idx):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
-    max_preds = preds.argmax(dim=1, keepdim=True)  # get the index of the max probability
-    non_pad_elements = (y != tag_pad_idx).nonzero()
-    correct = max_preds[non_pad_elements].squeeze(1).eq(y[non_pad_elements])
-    return correct.sum() / torch.FloatTensor([y[non_pad_elements].shape[0]])
-
-
-def train(model, iterator, optimizer, criterion, tag_pad_idx):
-    epoch_loss = 0
-    epoch_acc = 0
-    model.train()
-    for batch in iterator:
-        text = batch.word
-        tags = batch.tag
-        optimizer.zero_grad()
-        # text = [sent len, batch size]
-        predictions = model(text)
-        # predictions = [sent len, batch size, output dim]
-        # tags = [sent len, batch size]
-        predictions = predictions.view(-1, predictions.shape[-1])
-        tags = tags.view(-1)
-        # predictions = [sent len * batch size, output dim]
-        # tags = [sent len * batch size]
-        loss = criterion(predictions, tags)
-        acc = categorical_accuracy(predictions, tags, tag_pad_idx)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-        epoch_acc += acc.item()
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
-
-
-def evaluate(model, iterator, criterion, tag_pad_idx):
-    epoch_loss = 0
-    epoch_acc = 0
-    model.eval()
-    with torch.no_grad():
-        for batch in iterator:
+    def epoch(self):
+        epoch_loss = 0
+        epoch_acc = 0
+        self.model.train()
+        for batch in self.data.train_iter:
+            # text = [sent len, batch size]
             text = batch.word
-            tags = batch.tag
-            predictions = model(text)
-            predictions = predictions.view(-1, predictions.shape[-1])
-            tags = tags.view(-1)
-            loss = criterion(predictions, tags)
-            acc = categorical_accuracy(predictions, tags, tag_pad_idx)
-            epoch_loss += loss.item()
-            epoch_acc += acc.item()
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+            # tags = [sent len, batch size]
+            true_tags = batch.tag
+            self.optimizer.zero_grad()
+            pred_tags = self.model(text)
+            # to calculate the loss and accuracy, we flatten both prediction and true tags
+            # flatten pred_tags to [sent len, batch size, output dim]
+            pred_tags = pred_tags.view(-1, pred_tags.shape[-1])
+            # flatten true_tags to [sent len * batch size]
+            true_tags = true_tags.view(-1)
+            batch_loss = self.loss_fn(pred_tags, true_tags)
+            batch_acc = self.accuracy(pred_tags, true_tags)
+            batch_loss.backward()
+            self.optimizer.step()
+            epoch_loss += batch_loss.item()
+            epoch_acc += batch_acc.item()
+        return epoch_loss / len(self.data.train_iter), epoch_acc / len(self.data.train_iter)
 
+    def evaluate(self, iterator):
+        epoch_loss = 0
+        epoch_acc = 0
+        self.model.eval()
+        with torch.no_grad():
+            # similar to epoch() but model is in evaluation mode and no backprop
+            for batch in iterator:
+              text = batch.word
+              true_tags = batch.tag
+              pred_tags = self.model(text)
+              pred_tags = pred_tags.view(-1, pred_tags.shape[-1])
+              true_tags = true_tags.view(-1)
+              batch_loss = self.loss_fn(pred_tags, true_tags)
+              batch_acc = self.accuracy(pred_tags, true_tags)
+              epoch_loss += batch_loss.item()
+              epoch_acc += batch_acc.item()
+        return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
+    def train(self, n_epochs):
+        for epoch in range(n_epochs):
+            start_time = time.time()
+            train_loss, train_acc = self.epoch()
+            end_time = time.time()
+            epoch_mins, epoch_secs = NER.epoch_time(start_time, end_time)
+            print(f"Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s")
+            print(f"\tTrn Loss: {train_loss:.3f} | Trn Acc: {train_acc * 100:.2f}%")
+            val_loss, val_acc = self.evaluate(self.data.val_iter)
+            print(f"\tVal Loss: {val_loss:.3f} | Val Acc: {val_acc * 100:.2f}%")
+        test_loss, test_acc = self.evaluate(self.data.test_iter)
+        print(f"Test Loss: {test_loss:.3f} |  Test Acc: {test_acc * 100:.2f}%")
 
-
-def tag_sentence(model, sentence, text_field, tag_field):
-    model.eval()
-    if isinstance(sentence, str):
+    def infer(self, sentence):
+        self.model.eval()
+        # tokenize sentence
         nlp = Indonesian()
-        tokens = [token.text for token in nlp(sentence)]
-    else:
-        tokens = [token for token in sentence]
-    if text_field.lower:
-        tokens = [t.lower() for t in tokens]
-    numericalized_tokens = [text_field.vocab.stoi[t] for t in tokens]
-    unk_idx = text_field.vocab.stoi[text_field.unk_token]
-    unks = [t for t, n in zip(tokens, numericalized_tokens) if n == unk_idx]
-    token_tensor = torch.LongTensor(numericalized_tokens)
-    token_tensor = token_tensor.unsqueeze(-1)
-    predictions = model(token_tensor)
-    top_predictions = predictions.argmax(-1)
-    predicted_tags = [tag_field.vocab.itos[t.item()] for t in top_predictions]
-    return tokens, predicted_tags, unks
+        tokens = [token.text.lower() for token in nlp(sentence)]
+        # transform to indices based on corpus vocab
+        numericalized_tokens = [self.data.word_field.vocab.stoi[t] for t in tokens]
+        # find unknown words
+        unk_idx = self.data.word_field.vocab.stoi[self.data.word_field.unk_token]
+        unks = [t for t, n in zip(tokens, numericalized_tokens) if n == unk_idx]
+        # begin prediction
+        token_tensor = torch.LongTensor(numericalized_tokens)
+        token_tensor = token_tensor.unsqueeze(-1)
+        predictions = self.model(token_tensor)
+        # convert results to tags
+        top_predictions = predictions.argmax(-1)
+        predicted_tags = [self.data.tag_field.vocab.itos[t.item()] for t in top_predictions]
+        # print inferred tags
+        max_len = max([len(token) for token in tokens])
+        print(f"{'word'.ljust(max_len)}\t{'unk'.ljust(max_len)}\ttag")
+        for token, tag in zip(tokens, predicted_tags):
+            is_unk = "✓" if token in unks else ""
+            print(f"{token.ljust(max_len)}\t{is_unk.ljust(max_len)}\t{tag}")
+        return tokens, predicted_tags, unks
 
 
 if __name__ == "__main__":
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    WORD = Field(lower=True)
-    TAG = Field(unk_token=None)
-    train_dataset, test_dataset = SequenceTaggingDataset.splits(
-        path="../input",
-        train="train.tsv",
-        test="test.tsv",
-        fields=(("word", WORD), ("tag", TAG))
+    corpus = Corpus(
+        input_folder="../input",
+        min_word_freq=3,  # any words occurring less than 3 times will be ignored from vocab
+        batch_size=64
     )
-    WORD.build_vocab(train_dataset.word, min_freq=3)
-    TAG.build_vocab(train_dataset.tag)
-    train_iter, test_iter = BucketIterator.splits((train_dataset, test_dataset), batch_size=64, device=device)
-    PAD_IDX = WORD.vocab.stoi[WORD.pad_token]
-    TAG_PAD_IDX = TAG.vocab.stoi[TAG.pad_token]
-    EMBEDDING_DIM = 100
+    print(f"Train set: {len(corpus.train_dataset)} sentences")
+    print(f"Val set: {len(corpus.val_dataset)} sentences")
+    print(f"Test set: {len(corpus.test_dataset)} sentences")
     bilstm = BiLSTM(
-        input_dim=len(WORD.vocab),
-        embedding_dim=EMBEDDING_DIM,
+        input_dim=len(corpus.word_field.vocab),
+        embedding_dim=300,
         hidden_dim=64,
-        output_dim=len(TAG.vocab),
-        n_layers=2,
-        bidirectional=True,
-        dropout=0.25,
-        pad_idx=WORD.vocab.stoi[WORD.pad_token]
+        output_dim=len(corpus.tag_field.vocab),
+        lstm_layers=2,
+        emb_dropout=0.5,
+        lstm_dropout=0.1,
+        fc_dropout=0.25,
+        word_pad_idx=corpus.word_pad_idx
     )
-    bilstm.apply(init_weights)
-    print(f'The model has {count_parameters(bilstm):,} trainable parameters')
-    pretrained_embeddings = WORD.vocab.vectors
-    bilstm.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
-    adam = Adam(bilstm.parameters())
-    ce_loss = nn.CrossEntropyLoss(ignore_index=TAG_PAD_IDX)
-    N_EPOCHS = 15
-    for epoch in range(N_EPOCHS):
-        start_time = time.time()
-        train_loss, train_acc = train(bilstm, train_iter, adam, ce_loss, TAG_PAD_IDX)
-        end_time = time.time()
-        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-        print(f"Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s")
-        print(f"\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%")
-    test_loss, test_acc = evaluate(bilstm, test_iter, ce_loss, TAG_PAD_IDX)
-    print(f"Test Loss: {test_loss:.3f} |  Test Acc: {test_acc * 100:.2f}%")
-    torch.save(bilstm.state_dict(), "../pretrain/model/bilstm/simple.pt")
-    bilstm.load_state_dict(torch.load("../pretrain/model/bilstm/simple.pt"))
+    bilstm.init_weights()
+    bilstm.init_embeddings(tag_pad_idx=corpus.tag_pad_idx)
+    print(f"The model has {bilstm.count_parameters():,} trainable parameters.")
+    ner = NER(
+        model=bilstm,
+        data=corpus,
+        optimizer_cls=Adam,
+        loss_fn_cls=nn.CrossEntropyLoss
+    )
+    ner.train(11)
     sentence = "\"Menjatuhkan sanksi pemberhentian tetap kepada teradu Sophia Marlinda Djami selaku Ketua KPU Kabupaten Sumba Barat, sejak dibacakannya putusan ini\", ucap Alfitra dalam sidang putusan, Rabu (8/7/2020)."
-    tokens, pred_tags, unks = tag_sentence(bilstm, sentence.strip(), WORD, TAG)
-    print(tokens)
-    print(pred_tags)
-    print(unks)
-    for token, tag in zip(tokens, pred_tags):
-        print(f"{token}\t\t{tag}")
+    words, infer_tags, unknown_tokens = ner.infer(sentence=sentence)
--- nerindo/preprocessor.py
+++ nerindo/preprocessor.py
@@ -1,5 +1,6 @@
 import json
 import re
+import random
 from pprint import pprint
 from spacy.lang.id import Indonesian
 
@@ -52,11 +53,16 @@ def transform_json_to_conll():
     file = "../data/processed/test/test.json"
     with open(file, "r") as f:
         annotations = json.load(f)
-    buffer_conll = ""
-    for annotation in annotations:
+    random.seed(1339)
+    random.shuffle(annotations)
+    buffer_conll = {
+        "val": "",
+        "test": ""
+    }
+    for anno_i, annotation in enumerate(annotations):
         sorted_labels = sorted(annotation["labels"], key=lambda label: (label[0], label[1]))
-        i = 0
-        curr_label = sorted_labels[i] if len(sorted_labels) > 0 else None
+        token_i = 0
+        curr_label = sorted_labels[token_i] if len(sorted_labels) > 0 else None
         tokens = nlp(annotation["text"])
         for token in tokens:
             token_begin = token.idx
@@ -66,17 +72,19 @@ def transform_json_to_conll():
                 tag = curr_label[2]
                 if token_end == curr_label[1]:
                     tag = f"L-{tag}" if token_begin > curr_label[0] else f"U-{tag}"
-                    if i < len(sorted_labels) - 1:
-                        i += 1
-                        curr_label = sorted_labels[i]
+                    if token_i < len(sorted_labels) - 1:
+                        token_i += 1
+                        curr_label = sorted_labels[token_i]
                 elif token_begin == curr_label[0]:
                     tag = f"B-{tag}"
                 else:
                     tag = f"I-{tag}"
-            buffer_conll += token.text + "\t" + tag + "\n"
-        buffer_conll += "\n"
+            buffer_conll["val" if anno_i <= len(annotations) // 2 else "test"] += token.text + "\t" + tag + "\n"
+        buffer_conll["val" if anno_i <= len(annotations) // 2 else "test"] += "\n"
+    with open("../input/val.tsv", "w") as f:
+        f.write(buffer_conll["val"])
     with open("../input/test.tsv", "w") as f:
-        f.write(buffer_conll)
+        f.write(buffer_conll["test"])
 
 
 if __name__ == "__main__":
